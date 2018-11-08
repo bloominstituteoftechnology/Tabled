5abd3f00ffe18ee3e32f52ff4ccc873e4d934374
AustinBCole <austin.cole.chileno@gmail.com>
2018-11-08 16:21:28 -0700

    Implement cellForRow and numberOfRows

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -1,7 +1,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+        return cell
+    }
+    
 
 
     @IBOutlet weak var textField: UITextField!
@@ -13,6 +23,10 @@ class ViewController: UIViewController {
     
     
     @IBAction func add(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else {return}
+        Model.shared.addItem(text)
+        tableView.insertRows(at: [IndexPath(row: Model.shared.itemCount() - 1, section: 0)], with: .fade)
+        
     }
     
     let reuseIdentifier = "cell"

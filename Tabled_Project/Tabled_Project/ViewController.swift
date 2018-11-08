9361b46de72e28667230e8fc4cce64a5d5c8b976
Meera Andersen <meera.andersen@gmail.com>
2018-11-08 15:29:41 -0700

    set up UI data source and view delegate, and implemented number of rows.

--- Tabled_Project/Tabled_Project/ViewController.swift
+++ Tabled_Project/Tabled_Project/ViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
     
     
     @IBOutlet weak var textField: UITextField!
@@ -21,9 +23,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 
 
 }
 
+tableView.dataSource = self
+tableView.delegate = self

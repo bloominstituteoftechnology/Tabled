6f30642bfe043980a6ccc6f112f0f3dfb9c7df8b
AustinBCole <austin.cole.chileno@gmail.com>
2018-11-08 15:59:59 -0700

    Set up ViewController and create Model

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -4,5 +4,18 @@ import UIKit
 class ViewController: UIViewController {
 
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    let reuseIdentifier = "cell"
+    
 }
 

800da4b2a6555613801455981ae4890ea6088b5d
Sean Doyle <sdoylelambda@gmail.com>
2018-11-08 17:43:28 -0500

    add func

--- 11.8-Tabled/ViewController.swift
+++ 11.8-Tabled/ViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath)
+    }
+    
     
     let reuseIdentifier = "cell"
 
@@ -9,7 +18,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func add(_ sender: Any) {
-        //add code
+        guard let text = textField.text, !text.isEmpty else { return }
+        Model.shared.addItem(text)
+        textField.text = nil
+        tableView.insertRows(at: [IndexPath(row: Model.shared.addItem() - 1, section: 0)], with: .top)
     }
     
     

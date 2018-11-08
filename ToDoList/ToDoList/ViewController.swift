3c0e8f64918d3320831b3c041d0326e09a4982b2
jkaunert <jkaunert@clusterfly.net>
2018-11-08 16:50:57 -0600

    adds interface and constraints

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let reuseIdentifier = "cell"
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func add(_ sender: Any) {
+        
+    }
+    
+}
+

d675437951783d252e01ef15241e6858c27d53ed
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-08 14:35:31 -0700

    Initial setup for view controller complete. Outlets added & reuseIdentifier set to the cell.

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -1,7 +1,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
 }
+
+let reuseIdentifier = "cell"
 

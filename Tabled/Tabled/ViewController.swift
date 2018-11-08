42b9d59f00c5947ccca5385056889295487840ee
Stuart Sippel <stuartsippel@icloud.com>
2018-11-08 16:22:05 -0600

    Add base xcode proj UI element, outlets, actions, and Model file

---
+++ Tabled/Tabled/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tabled
+//
+//  Created by Stuart on 11/8/18.
+//  Copyright © 2018 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    let reuseIdentifier = "cell"
+}
+

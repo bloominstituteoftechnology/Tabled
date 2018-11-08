70b57dd7dfe58086c9f326170ef5ca1d4e41d632
Frulwinn <frulwinn@gmail.com>
2018-11-08 14:19:55 -0800

    set up project in interface builder and made ib outlets and ib actions

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
 
+let reuseIdentifier = "cell"
+
 class ViewController: UIViewController {
-
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func add(_ sender: Any) {
+        
+    }
+    
 }
 

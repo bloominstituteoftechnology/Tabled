c80495997790e2af827e904bb96fb8d782e5bdb6
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-08 15:01:01 -0700

    Applied insert rows animation. Set the reuseIdentifier to cell, implemented viewWillAppear and reloaded the data. Added swipe-to-delete functionality.

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -1,12 +1,52 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBAction func add(_ sender: Any) {
+        
+        guard let text = textField.text, !text.isEmpty else { return }
+        
+        Model.shared.addItem(text)
+        
+        textField.text = nil
+        
+        tableView.insertRows(at: [IndexPath(row: Model.shared.itemCount() - 1, section: 0)], with: .top)
 
 }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+    }
 
-let reuseIdentifier = "cell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    let reuseIdentifier = "cell"
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard editingStyle == .delete else { return }
+        
+        Model.shared.removeItem(at: indexPath.row)
+        
+        tableView.deleteRows(at: [indexPath], with: .left)
+    }
 
+}

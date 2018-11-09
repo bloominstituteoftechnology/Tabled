5b4e25d0f31496934ad9f2982efded768902d3ff
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-08 18:40:33 -0800

    Build The View Controller: Completed

--- Tabled Project/Tabled Project/ViewController.swift
+++ Tabled Project/Tabled Project/ViewController.swift
@@ -1,31 +1,72 @@
-//
-//  ViewController.swift
-//  Tabled Project
-//
-//  Created by Ivan Caldwell on 11/8/18.
-//  Copyright © 2018 Ivan Caldwell. All rights reserved.
-//
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-   var reuseIdentifier = "cell"
+   let reuseIdentifier = "cell"
     
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var tableView: UITableView!
     
-    
     @IBAction func add(_ sender: Any) {
+        
+        guard let text = textField.text, !text.isEmpty else {return}
+        // add to model
+        Model.shared.addItem(text)
+        
+        // remove the entry from the text field
+        textField.text = nil
+        
+        // So we can see the new cell.
+        //tableView.reloadData()
+        tableView.insertRows(at: [IndexPath(row: Model.shared.itemCount() - 1, section: 0)], with: .top)
+    
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+        return cell
+    }
+    
+    ///////////////// DELETE FUNCTIONALITY ////////////////////////
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // Only handle deletions
+        guard editingStyle == .delete else { return }
+        Model.shared.removeItem(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    
+    /////////////// EDIT BUTTON FUNCTIONALITY ///////////////////////
+    @IBAction func editTable(_ sender: Any) {
+        tableView.setEditing(true, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
+        
+    }
+    
+    @objc
+    func stopEditingTable(_ sender: Any) {
+        tableView.setEditing(false, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTable(_:)))
+    }
+    
+    /////////////// MOVE FUNCTIONALITY ///////////////////////////////
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        Model.shared.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+    }
+    
 
 }
 

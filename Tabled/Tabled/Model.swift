1ddf1ad0e1091c4bf1ef3b7c5b446d1ec4ef13d7
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-08 20:47:19 -0500

    Create an app that allows users to create a todo list

---
+++ Tabled/Tabled/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Tabled
+//
+//  Created by Julian A. Fordyce on 11/8/18.
+//  Copyright © 2018 Glas Labs. All rights reserved.
+//
+
+import Foundation
---
+++ Tabled/Tabled/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabled
+//
+//  Created by Julian A. Fordyce on 11/8/18.
+//  Copyright © 2018 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

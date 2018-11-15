1d3a901c7708f858d67880dcebd09d8191ee0182
Stuart Sippel <stuartsippel@icloud.com>
2018-11-15 15:50:22 -0600

    build view contoller with add button, conform to UITableViewDataSource and UITableViewDelegate, implement viewWillAppear, row-reordering, and interactive edits

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -7,7 +7,18 @@
 //
 
 import UIKit
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+        return cell
+    }
+    
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -17,17 +28,55 @@ class ViewController: UIViewController {
     
     
     @IBAction func add(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+        
+        Model.shared.addItem(text)
+        textField.text = nil
+        tableView.reloadData()
     }
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+        
+    }
+
     
     let reuseIdentifier = "cell"
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // swipe to delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // Enable "magic" swipe-to-delete
+        guard editingStyle == .delete else { return }
+        
+        Model.shared.removeItem(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
     }
 
+    // row-reordering
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        Model.shared.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
 
+    // introduces interactive edits
+    @IBAction func editTable(_ sender: Any) {
+        tableView.setEditing(true, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
+    }
+    
+    @objc
+    func stopEditingTable(_ sender: Any) {
+        tableView.setEditing(false, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTable(_:)))
+    }
+    
+    
 }
 

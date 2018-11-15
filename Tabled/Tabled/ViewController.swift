dcfa68d9fddba9435ff4e0424d72b4677cd6c14c
Sameera Leola <sameeraleola@gmail.com>
2018-11-15 16:13:33 -0600

    Troubleshooting interactive edit

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -15,10 +15,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     let reuseIdentifier = "cell"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Setting the viewController as dataSource and delegate to conform to UITableViewDataSource and UITableViewDelegate
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
     }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
     
     @IBAction func add(_ sender: Any) {
         guard let newToDo = textField.text, !newToDo.isEmpty else {return}
@@ -31,19 +39,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return Model.shared.itemCount()
     }
     
-    //Setting the viewController as dataSource and delegate to conform to UITableViewDataSource and UITableViewDelegate
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.reloadData()
-    }
-    
     //Get the item at the specified index
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        let todoItem = Model.shared.item(at: indexPath.row)
-        cell.textLabel?.text = todoItem
+      // let todoItem = Model.shared.item(at: indexPath.row)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+       // cell.textLabel?.text = todoItem
         return cell
     }
     
@@ -57,6 +58,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.deleteRows(at: [indexPath], with: .automatic)
     }
     
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        Model.shared.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+    }
+    
+    @IBAction func editTable(_ sender: Any) {
+        setEditing(true, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
+    }
+    
+    @objc
+    func stopEditingTable(_ sender: Any) {
+        tableView.setEditing(false, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTable(_:)))
+    }
     
 
 }

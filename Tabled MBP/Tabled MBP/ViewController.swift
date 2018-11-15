fd4c2fb6e02245fc6c73d479c7af5bd838554059
Audrey Welch <welch.audreyl@gmail.com>
2018-11-15 14:50:56 -0600

    Set up ViewController.swift file

--- Tabled MBP/Tabled MBP/ViewController.swift
+++ Tabled MBP/Tabled MBP/ViewController.swift
@@ -8,11 +8,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let reuseIdentifier = "cell"
+    
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 }
 

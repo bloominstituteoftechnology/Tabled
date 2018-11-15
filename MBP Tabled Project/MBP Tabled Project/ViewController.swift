58da24d1f2a570adabe051ee59ba4ec7e6e0f266
Meera Andersen <meera.andersen@gmail.com>
2018-11-15 13:35:40 -0700

    built out IB to specifications

---
+++ MBP Tabled Project/MBP Tabled Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MBP Tabled Project
+//
+//  Created by Meera Andersen on 11/15/18.
+//  Copyright © 2018 Meera Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

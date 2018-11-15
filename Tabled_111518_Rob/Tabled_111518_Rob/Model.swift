a691a7bf229e680a3913b5c3acf44074e8b60481
Rob Herold <rderek@bresnan.net>
2018-11-15 13:54:12 -0700

    initial commit, IB portion complete, Model.swift added

---
+++ Tabled_111518_Rob/Tabled_111518_Rob/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Tabled_111518_Rob
+//
+//  Created by Rob Herold on 11/15/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import Foundation
---
+++ Tabled_111518_Rob/Tabled_111518_Rob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabled_111518_Rob
+//
+//  Created by Rob Herold on 11/15/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

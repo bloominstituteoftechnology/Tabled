76510ec74c2945d077220b0791e44983f8b88491
Sean Doyle <sdoylelambda@gmail.com>
2018-11-15 15:38:42 -0500

    storyboard added

---
+++ Tabled/Tabled/Model.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Model {
+    let shared = Model()
+    private init() {}
+    
+    private var items: [String] = []
+    
+    func addItem(_ item: String) {
+        
+    }
+    
+}
--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -1,8 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let reuseIdentifier = "cell"
 
- 
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UIView!
+    
+    
+    @IBAction func add(_ sender: Any) {
+        
+    }
+    
 }
 

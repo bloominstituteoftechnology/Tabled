bed09fbcdcbd301ad6770e83bd8bb8a52753edcd
Sean Doyle <sdoylelambda@gmail.com>
2019-01-10 15:55:57 -0500

    Model with add and delete completed.

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -1,7 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let reuseIdentifier = "TabledCell"
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

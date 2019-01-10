c01d95698055d7cf9d5fe1d613d4021a48a46b28
Paul Yi <yipaulc@gmail.com>
2019-01-10 13:19:19 -0800

    Set up ViewController.swift

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var textField: UITextField!
+    
+    let reuseIdentifier = "cell"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
 
 
 }

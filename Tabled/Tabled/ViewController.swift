cf6787e2515ba1a12b7c40820f356eefb0dc746f
Stuart Sippel <stuartsippel@icloud.com>
2019-01-10 16:39:11 -0600

    Set delegate, implement numberOFRows and cellForRowAt

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBAction func add(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+        
+        Model.shared.addItem(text)
+        textField.text = nil
+        tableView.reloadData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+        return cell
     }
 
     @IBOutlet weak var textField: UITextField!

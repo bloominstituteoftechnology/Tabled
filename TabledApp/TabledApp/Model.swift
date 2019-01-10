72145aa995683a9fafc5ce74b8791ab87d1d3283
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-10 15:19:49 -0500

    Add model

---
+++ TabledApp/TabledApp/Model.swift
@@ -0,0 +1,52 @@
+//
+//  Model.swift
+//  TabledApp
+//
+//  Created by Nelson Gonzalez on 1/10/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+
+import Foundation
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    private var items: [String] = []
+    
+    func addItem(_ item: String) {
+        // fill this in
+    }
+    
+    func removeItem(at index: Int) {
+        // fill this in
+    }
+    
+    func moveItem(from index: Int, to destinationIndex: Int) {
+        // fill this in
+    }
+    
+    func itemCount() -> Int {
+        return items.count
+    }
+    
+    func item(at index: Int) -> String {
+        return items[index]
+    }
+    
+    let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+        .appendingPathComponent("Library")
+        .appendingPathComponent("ToDo")
+        .appendingPathExtension("plist")
+    
+    func saveData() {
+        try! (items as NSArray).write(to: fileURL)
+    }
+    
+    func loadData() {
+        if let items = NSArray(contentsOf: fileURL) as? [String] {
+            items = items
+        }
+    }
+}
--- TabledApp/TabledApp/ViewController.swift
+++ TabledApp/TabledApp/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let reuseIdentifier = "cell"
 
     @IBOutlet weak var tableView: UITableView!
     

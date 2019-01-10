e00ef0103dc7a597ad6b405170c09764a82c7c1c
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-01-10 15:52:39 -0500

    will show initial view controller with table, textfield, add button, and navigation controller

---
+++ Tabled/Tabled/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabled
+//
+//  Created by Jocelyn Stuart on 1/10/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

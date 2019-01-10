33b119bb3ac5597e01a6f115893506b40e587926
“Cameron <cameron.dunn2013@gmail.com>
2019-01-10 14:30:13 -0600

    -Make app have the UI elements as described in the instructions. -Began prepping for project to use a dictionary to store information regarding the TableView.

--- Tabled/Tabled/ViewController.swift
+++ Tabled/Tabled/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    @IBOutlet weak var textField : UITextField!
+    let itemDict : [String : Int] = [:]
+    let reuseIdentifier : String = "cell"
+    
+    
+    @IBAction func add(_ sender: Any) {
+        if(textField.text != nil && !textField.text!.isEmpty && textField.text != " " && !textField.text!.contains("  ")){
+            Model.shared.addItem(textField.text!)
+            tableView.reloadData()
+        }else{
+            let alert = UIAlertController(title: "Error", message: "Item cannot be blank and/or contain more than two spaces in a row.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default))
+            self.present(alert, animated: true)
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)
+        return cell!
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        //input what will happen when view will appear
+    }
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //input what will happen when view is loaded
     }
 
 

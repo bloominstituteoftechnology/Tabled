c0d1a2c8de86dd59d580cb87b9caf9063873218e
Angel <angelbuenrostro@outlook.com>
2019-01-10 17:32:06 -0500

    Implement todo cell in the tableview

--- Tabled/Tabled/View Controllers/ViewController.swift
+++ Tabled/Tabled/View Controllers/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+        cell.textLabel!.text = Model.shared.item(at: indexPath.row)
+        return cell
+    }
+    
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
     
     
     @IBAction func add(_ sender: UIButton) {
+        guard let text = textField.text, !text.isEmpty else { return }
+        Model.shared.addItem(text)
+        print(Model.shared.item(at: 0))
+        tableView.insertRows(at: [IndexPath(row: Model.shared.itemCount()-1, section: 0)], with: .right)
+        textField.text = nil
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.reloadData()
     }
     
     let reuseIdentifier = "cell"

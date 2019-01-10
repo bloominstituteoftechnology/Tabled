5394bc406fb149df6d20ba9e1954222434aae86a
Angel <angelbuenrostro@outlook.com>
2019-01-10 18:40:52 -0500

    Add a second view to implement a share feature which allows the user to email their todo list

---
+++ Tabled/Tabled/View Controllers/ShareViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ShareViewController.swift
+//  Tabled
+//
+//  Created by Angel Buenrostro on 1/10/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    @IBOutlet weak var subject: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

63ff97b4d9f48f84861b451eeba539689fbaf19e
Stuart Sippel <stuartsippel@icloud.com>
2019-01-10 16:22:06 -0600

    Build Model/Controller

---
+++ Tabled/Tabled/Model.swift
@@ -0,0 +1,52 @@
+//
+//  Model.swift
+//  Tabled
+//
+//  Created by Stuart on 1/10/19.
+//  Copyright © 2019 Stuart. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    private var items: [String] = []
+    
+    func addItem(_ item: String) {
+        items.append(item)
+    }
+    
+    func removeItem(at index: Int) {
+        items.remove(at: index)
+    }
+    
+    func moveItem(from index: Int, to destinationIndex: Int) {
+        let item = items.remove(at: index)
+        items.insert(item, at: destinationIndex)
+    }
+    
+    func itemCount() -> Int {
+        return items.count
+    }
+    
+    func item(at index: Int) -> String {
+        return items[index]
+    }
+    
+    let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+        .appendingPathComponent("Library")
+        .appendingPathComponent("ToDo")
+        .appendingPathExtension("plist")
+    
+    func saveData() {
+        try! (items as NSArray).write(to: fileURL)
+    }
+    
+    func loadData() {
+        if let items = NSArray(contentsOf: fileURL) as? [String] {
+            items = items
+        }
+    }
+}

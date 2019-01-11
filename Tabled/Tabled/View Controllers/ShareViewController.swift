a278c103f8f21610aab85b08fe3517f575f2ccab
Angel <angelbuenrostro@outlook.com>
2019-01-10 19:16:35 -0500

    Add the email functionality

--- Tabled/Tabled/View Controllers/ShareViewController.swift
+++ Tabled/Tabled/View Controllers/ShareViewController.swift
@@ -15,16 +15,11 @@ class ShareViewController: UIViewController, MFMailComposeViewControllerDelegate
     @IBOutlet weak var bodyTextView: UITextView!
     
     @IBAction func send(_ sender: UIButton) {
-        let picker = MFMailComposeViewController()
-        picker.mailComposeDelegate = self
-        
-        if let subjectText = subjectTextField.text {
-            picker.setSubject(subjectText)
-        }
-        picker.setMessageBody(bodyTextView.text, isHTML: true)
-        
-        present(picker, animated: true, completion: nil)
+        sendEmail()
     }
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,45 +35,21 @@ class ShareViewController: UIViewController, MFMailComposeViewControllerDelegate
     }
     
     
-    
-    // MFMailComposeViewControllerDelegate
-    
-    // 1
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // UITextFieldDelegate
-    
-    // 2
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        
-        return true
-    }
-    
-    // UITextViewDelegate
-    
-    // 3
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        bodyTextView.text = textView.text
-        
-        if text == "\n" {
-            textView.resignFirstResponder()
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["you@yoursite.com"])
+            mail.setMessageBody(bodyTextView.text, isHTML: true)
             
-            return false
+            present(mail, animated: true)
+        } else {
+            // show failure alert
         }
-        
-        return true
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
-    */
 
 }

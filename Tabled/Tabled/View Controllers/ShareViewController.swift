9847f54850fdac59f00adba31ab7c9e81d040efe
Angel <angelbuenrostro@outlook.com>
2019-01-10 19:06:24 -0500

    Put todo list into email body automatically whenever the share view is accessed

--- Tabled/Tabled/View Controllers/ShareViewController.swift
+++ Tabled/Tabled/View Controllers/ShareViewController.swift
@@ -7,17 +7,70 @@
 //
 
 import UIKit
+import MessageUI
 
-class ShareViewController: UIViewController {
+class ShareViewController: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
 
-    @IBOutlet weak var subject: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var subjectTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    @IBAction func send(_ sender: UIButton) {
+        let picker = MFMailComposeViewController()
+        picker.mailComposeDelegate = self
+        
+        if let subjectText = subjectTextField.text {
+            picker.setSubject(subjectText)
+        }
+        picker.setMessageBody(bodyTextView.text, isHTML: true)
+        
+        present(picker, animated: true, completion: nil)
     }
     
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subjectTextField.delegate = self
+        bodyTextView.delegate = self
+        var i = 0
+        var todoText : String = ""
+        while i < Model.shared.itemCount(){
+            todoText += ("\(Model.shared.item(at: i))" + "\n")
+            i += 1
+        }
+        bodyTextView.text = todoText
+    }
+    
+    
+    
+    // MFMailComposeViewControllerDelegate
+    
+    // 1
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // UITextFieldDelegate
+    
+    // 2
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // UITextViewDelegate
+    
+    // 3
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        bodyTextView.text = textView.text
+        
+        if text == "\n" {
+            textView.resignFirstResponder()
+            
+            return false
+        }
+        
+        return true
+    }
     /*
     // MARK: - Navigation
 
